fc1c3472d54a5cc23771313539e3baf1a424dc72
Ayush Sharma <ayushsharma@Ayushs-MacBook-Air-2.local>
2024-04-25 00:16:42 -0500

    Terraform changes

--- terraform/init_script.sh
+++ terraform/init_script.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+
+exec > >(tee ~ubuntu/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash &&
 source ~/.nvm/nvm.sh &&
-nvm install --lts &&
+nvm install 17.3.0 &&
 
 # Clone the repo form github on the EC2 instance
 mkdir resper_frontend && cd resper_frontend &&
-git clone https://github.com/asharm10/Front-End-With-Veutify.git &&
-cd Front-End-With-Veutify && 
+git clone https://github.com/asharm10/Resper_Frontend_Cloud.git &&
+cd Resper_Frontend_Cloud && 
 
-npm install && screen -d -m npm run build && sleep 10
+npm install && npm run serve
